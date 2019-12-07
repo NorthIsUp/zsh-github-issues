c03e221a6e64333e929c9083843be236d1dc46bc
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-12-07 23:47:20 +0100

    plugin.zsh: Update the $0 handling according to the standard

--- zsh-github-issues.plugin.zsh
+++ zsh-github-issues.plugin.zsh
@@ -1,6 +1,7 @@
 # According to the Zsh Plugin Standard:
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
 typeset -g CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/zsh-github-issues"
 typeset -g CACHE_SEEN_IDS="$CACHE_DIR/ids_seen.dat"
